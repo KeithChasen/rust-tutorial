9b566313d4047d5034d004bdcbe4db41baa847f4
keithchasen <keithchasen89@gmail.com>
2022-03-29 00:21:43 +0300

    printing wit positional arguments

--- src/print.rs
+++ src/print.rs
@@ -2,4 +2,7 @@ pub fn run() {
     println!("Hello from print.rs");
     println!("Number: {}", 1);
     println!("My name is {} I'm {}", "Kostya", 33);
+
+    // Positional Argument
+    println!("I am a {0}-developer and {1} can be used for {0}-development", "web", "Rust");
 }
