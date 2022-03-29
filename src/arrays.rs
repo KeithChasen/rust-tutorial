b8a2ab0099d1fb8a33fca632d7821a9e85539240
keithchasen <keithchasen89@gmail.com>
2022-03-29 22:10:33 +0300

    arrays

---
+++ src/arrays.rs
@@ -0,0 +1,20 @@
+// arrays: fixed length and same data type
+
+pub fn run() {
+    let mut numbers: [i32; 5] = [1,2,3,4,5];
+
+    println!("{:?}", numbers);
+
+    // get single val
+    println!("{}", numbers[0]);
+
+    //reassign value
+    numbers[2] = 20;
+    println!("{:?}", numbers);
+
+    //get array length
+    println!("array length: {}", numbers.len());
+
+    //arrays are stack allocated
+    println!("array occupies: {} bytes", std::mem::size_of_val(&numbers));
+}
