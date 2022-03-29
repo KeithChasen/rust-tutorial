805d6f083a0ef705376313f6e50a2553247c7a32
keithchasen <keithchasen89@gmail.com>
2022-03-29 18:08:45 +0300

    some string methods

--- src/string.rs
+++ src/string.rs
@@ -14,5 +14,22 @@ pub fn run() {
     //add string
     phrase.push_str("ostya");
 
+    //capacity in bytes
+    println!("Capacity: {}", phrase.capacity());
+
+    // is empty
+    println!("Is empty: {}", phrase.is_empty());
+
+    // contains substring
+    println!("Contains 'Kostya': {}", phrase.contains("Kostya"));
+
+    //replace
+    println!("Replace: {}", phrase.replace("Kostya", "dude"));
+
+    // loop through string
+    for word in phrase.split_whitespace() {
+        println!("{}", word);
+    }
+
     println!("{} {}", hello, phrase);
 }
