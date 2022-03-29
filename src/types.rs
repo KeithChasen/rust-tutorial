4a8482a603f59806a2546c983610c070b450a823
keithchasen <keithchasen89@gmail.com>
2022-03-29 16:50:49 +0300

    boolean

--- src/types.rs
+++ src/types.rs
@@ -11,4 +11,14 @@ pub fn run() {
     // find max size
     println!("Max i32: {}", std::i32::MAX);
     println!("Max i64: {}", std::i64::MAX);
+
+    //Boolean
+    let is_active = true;
+    
+    let is_it: bool = false;
+
+    // get boolean from expression
+    let is_greater = 10 > 5;
+
+    println!("{:?}", (x,y,a,is_active, is_it, is_greater))
 }
