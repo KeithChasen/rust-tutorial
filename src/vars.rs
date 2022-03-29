d631206253fe7a909a15e9338e57eca2a44d9009
keithchasen <keithchasen89@gmail.com>
2022-03-29 15:43:02 +0300

    basic variables

---
+++ src/vars.rs
@@ -0,0 +1,10 @@
+pub fn run() {
+    let name = "Kostya";
+
+    let mut age = 32;
+    println!("Last year I was {}", age);
+    
+    age = 33;
+
+    println!("My name is {}. I am {}", name, age);
+}
