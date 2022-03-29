c0042db20a458fe20e16ad4d60838cfa1d3eaa88
keithchasen <keithchasen89@gmail.com>
2022-03-29 22:13:22 +0300

    array slices

--- src/arrays.rs
+++ src/arrays.rs
@@ -17,4 +17,8 @@ pub fn run() {
 
     //arrays are stack allocated
     println!("array occupies: {} bytes", std::mem::size_of_val(&numbers));
+
+    // get slice
+    let slice: &[i32] = &numbers[0..2];
+    println!("Slice: {:?}", slice);
 }
